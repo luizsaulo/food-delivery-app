95e711fa1f606fcb53fbf2722ee0886d61a218e4
Luiz <luizsaulo@yahoo.com.br>
2022-05-27 16:43:21 -0300

    fixing the header componenet with routes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
-ReactDOM.render(
+ReactDOM.render(  
   <Router>
     <App />
-  </Router>,
-
-  document.getElementById("root")
+  </Router>, 
+    document.getElementById("root")
 );
