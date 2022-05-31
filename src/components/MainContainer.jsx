8fdbf9b1df0a7b084af2e3d9bcac6fbbe2ff9cbc
Luiz <luizsaulo@yahoo.com.br>
2022-05-31 16:06:01 -0300

    building the main container component including the mobile screen and responsiveness

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import Delivery from '../img/delivery.png';
+import React from "react";
+import Delivery from "../img/delivery.png";
 
 const MainContainer = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
-      <div className='py-2 flex-1 flex flex-col items-start md:items-center justify-center'>
-        <div className='flex items-center gap-2 justify-center bg-orange-100 px-2 py-1 rounded-full'>
-          <p className='text-base text-orange-500 font-semibold'>Bike Delivery</p>
-          <div className='w-8 h-8 bg-white rounded-full overflow-hidden'>
-            <img src={Delivery} className='w-full h-full object-contain' alt='delivery' />
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">
+            Bike Delivery
+          </p>
+          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
+            <img
+              src={Delivery}
+              className="w-full h-full object-contain"
+              alt="delivery"
+            />
           </div>
         </div>
-        
-      </div>
-      <div className='py-2 bg-blue-400 flex-1'></div>
-    </div>
-  )
-}
 
-export default MainContainer
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+          The Fastest Delivery in
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">Your City</span>
+        </p>
+
+        <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+          voluptates veritatis asperiores expedita, similique praesentium
+          officia eos facilis voluptatum fugit, tenetur sapiente, mollitia iure
+          id culpa a officiis magnam amet!
+        </p>
+
+        <button
+          type="button"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 
+          rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
+          Order Now
+        </button>
+      </div>
+      <div className="py-2 bg-blue-400 flex-1"></div>
+    </div>
+  );
+};
+
+export default MainContainer;
