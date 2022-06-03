272d192bb106621536e46daa1b8791b1c6d24111
Luiz <luizsaulo@yahoo.com.br>
2022-06-03 16:22:00 -0300

    finishing the home container adding a map to load the information about the dishes that will be displayed on the front page, also implemented the responsiveness of these items

---
+++ src/utils/data.js
@@ -0,0 +1,11 @@
+import I1 from '../img/i1.png';
+import F1 from '../img/f1.png';
+import C3 from '../img/c3.png';
+import Fi1 from '../img/fi1.png';
+
+export const heroData = [
+    {id : 1, name : 'Icecream', decp : 'Chocolate & Vanilla', price : '5.99', imageScr : I1},
+    {id : 2, name : 'Scrawberries', decp : 'Fresh Strawberries', price : '4.99', imageScr : F1},
+    {id : 3, name : 'Chicken Kebab', decp : 'Mixed Kebab Plate', price : '8.99', imageScr : C3},
+    {id : 4, name : 'Fish Kebab', decp : 'Mix Fish Kebab', price : '10.99', imageScr : Fi1},
+  ]
