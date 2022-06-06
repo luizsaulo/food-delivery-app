e361c9334c3b7c803ae4b7b6ddc5c1a02bd1ebfe
Luiz <luizsaulo@yahoo.com.br>
2022-06-06 15:31:43 -0300

    continuing building the create container component inlcluding a Loader

--- src/utils/data.js
+++ src/utils/data.js
@@ -9,3 +9,41 @@ export const heroData = [
     {id : 3, name : 'Chicken Kebab', decp : 'Mixed Kebab Plate', price : '8.99', imageScr : C3},
     {id : 4, name : 'Fish Kebab', decp : 'Mix Fish Kebab', price : '10.99', imageScr : Fi1},
   ]
+
+  export const categories = [
+    {
+      id: 1,
+      name: 'Chicken',
+      urlParamName: 'chicken',
+    },
+    {
+      id: 2,
+      name: 'Curry',
+      urlParamName: 'curry',
+    },
+    {
+      id: 3,
+      name: 'Rice',
+      urlParamName: 'rice',
+    },
+    {
+      id: 4,
+      name: 'Fish',
+      urlParamName: 'fish',
+    },
+    {
+      id: 5,
+      name: 'Fruits',
+      urlParamName: 'fruits',
+    },
+    {
+      id: 6,
+      name: 'Icecreams',
+      urlParamName: 'icecreams',
+    },    
+    {
+      id: 7,
+      name: 'Soft Drinks',
+      urlParamName: 'drinks',
+    }
+  ]
