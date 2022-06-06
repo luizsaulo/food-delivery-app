76c2fdf4f5c2b7418a8882386b68a31500eeba9e
Luiz <luizsaulo@yahoo.com.br>
2022-06-06 14:32:58 -0300

    fixing a coupld of problems on the header component and starting to build the create container component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,8 +35,8 @@ const Header = () => {
   };
 
   const logout = () => {
-      setIsMenu(false)
-      localStorage.clear()
+      setIsMenu(false);
+      localStorage.clear();
 
       dispatch({
           type: actionType.SET_USER,
@@ -98,7 +98,7 @@ const Header = () => {
               >
                 {user && user.email === "teste@teste.com" && (
                   <Link to={"/createItem"}>
-                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base" onClick={() => setIsMenu(false)}>
                       New Item <MdAdd />
                     </p>
                   </Link>
@@ -154,16 +154,16 @@ const Header = () => {
               )}
 
               <ul className="flex flex-col">
-                <li className="text-base text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                <li className="text-base text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2" onClick={() => setIsMenu(false)}>
                   Home
                 </li>
-                <li className="text-base text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                <li className="text-base text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2" onClick={() => setIsMenu(false)}>
                   Menu
                 </li>
-                <li className="text-base text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                <li className="text-base text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2" onClick={() => setIsMenu(false)}>
                   About Us
                 </li>
-                <li className="text-base text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                <li className="text-base text-textColor hover: text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2" onClick={() => setIsMenu(false)}>
                   Service
                 </li>
               </ul>
--- src/utils/data.js
+++ src/utils/data.js
@@ -5,7 +5,7 @@ import Fi1 from '../img/fi1.png';
 
 export const heroData = [
     {id : 1, name : 'Icecream', decp : 'Chocolate & Vanilla', price : '5.99', imageScr : I1},
-    {id : 2, name : 'Scrawberries', decp : 'Fresh Strawberries', price : '4.99', imageScr : F1},
+    {id : 2, name : 'Strawberries', decp : 'Fresh Strawberries', price : '4.99', imageScr : F1},
     {id : 3, name : 'Chicken Kebab', decp : 'Mixed Kebab Plate', price : '8.99', imageScr : C3},
     {id : 4, name : 'Fish Kebab', decp : 'Mix Fish Kebab', price : '10.99', imageScr : Fi1},
   ]
