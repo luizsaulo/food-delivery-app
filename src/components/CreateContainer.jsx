263ea6694f5b4efb5189254be6f795e97b21129b
Luiz <luizsaulo@yahoo.com.br>
2022-06-06 16:24:10 -0300

    continuing with createcontainer component production; now we created a few more properties such as the calories and price input fields with the save button and their functions

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { motion } from 'framer-motion';
 import { categories } from "../utils/data";
 
-import { MdFastfood, MdCloudUpload, MdDelete } from 'react-icons/md';
+import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from 'react-icons/md';
 import Loader from './Loader';
 
 const CreateContainer = () => {
@@ -17,9 +17,12 @@ const CreateContainer = () => {
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const uploadImage = () => {}
+  const uploadImage = () => {};
 
-  const deleteImage = () => {}
+  const deleteImage = () => {};
+
+  const saveDetails = () => {};
+
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center'>
@@ -48,7 +51,7 @@ const CreateContainer = () => {
                 required value={title} 
                 onChange={(e) => setTitle(e.target.value)}
                 placeholder='Give me a title'
-                className='w-full h-full text-lg bg-transparent font-semibold
+                className='w-full h-full text-lg bg-transparent
                 outline-none border-none placeholder:text-gray-400 
                 text-textColor' 
               />
@@ -123,6 +126,46 @@ const CreateContainer = () => {
               )}
             </>
             )}
+          </div>
+
+          <div className='w-full flex flex-col md:flex-row items-center gap-3'>
+            <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+              <MdFoodBank className='text-gray-700 text-2xl' />
+              <input 
+                type='text' 
+                required
+                value={calories} 
+                onChange={(e) => setCalories(e.target.value)}
+                placeholder='Calories' 
+                className='w-full h-full text-lg bg-transparent outline-none 
+                border-none placeholder:text-gray-400 text-textColor' 
+              />
+            </div>
+
+            <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+              <MdAttachMoney className='text-gray-700 text-2xl' />
+              <input 
+                type='text' 
+                required 
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                placeholder='Price' 
+                className='w-full h-full text-lg bg-transparent outline-none 
+                border-none placeholder:text-gray-400 text-textColor' 
+              />
+            </div>
+          </div>
+
+          <div className='flex items-center w-full'>
+            <button 
+              type='button' 
+              className='ml-0 md:ml-auto w-full md:w-auto 
+              border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg
+              text-lg text-white font-semibold' 
+              onClick={saveDetails}
+            >
+              Save
+            </button>
           </div>
       </div>
     </div>
