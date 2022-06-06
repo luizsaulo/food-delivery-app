2f1ea479110510043f66f08d1a7c2cb916d3db5d
Luiz <luizsaulo@yahoo.com.br>
2022-06-06 15:57:40 -0300

    creating the upload image and delete image functions

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import { motion } from 'framer-motion';
 import { categories } from "../utils/data";
 
-import { MdFastfood } from 'react-icons/md';
+import { MdFastfood, MdCloudUpload, MdDelete } from 'react-icons/md';
+import Loader from './Loader';
 
 const CreateContainer = () => {
 
@@ -15,6 +16,10 @@ const CreateContainer = () => {
   const [alertStatus, setAlertStatus] = useState('danger');
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const uploadImage = () => {}
+
+  const deleteImage = () => {}
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center'>
@@ -73,11 +78,55 @@ const CreateContainer = () => {
           <div className='group flex justify-center items-center flex-col 
           border-2 border-dotted border-gray-300 w-full h-225 md:h-420 
           cursor-pointer rounded-lg'>
-            {isLoading ? <Loader /> : <></>}
+            {isLoading ? (
+            <Loader /> 
+            ) : (
+              <>
+                {!imageAsset ? (
+                <>
+                <label className='w=full h-full flex-col items-center justify-center cursor-pointer'>
+                  <div className='w=full h-full flex-col items-center justify-center gap-2'>
+                    <MdCloudUpload className='text-gray-500 text-3xl hover:text-gray-700' />
+                    <p className='text-gray-500 hover:text-gray-700'>
+                      Click here to upload
+                    </p>
+                  </div>
+                  <input 
+                    type='file' 
+                    name='uploadimage' 
+                    accept='image/*'
+                    onChange={uploadImage} 
+                    className='w-0 h-0' 
+                  />
+                </label>
+                </>
+                ) : (
+                <>
+                  <div className='relative h-full'>
+                    <img 
+                      src={imageAsset} 
+                      alt='uploaded image' 
+                      className='w-full h-full object-cover' 
+                    />
+                    <button 
+                      type='button' 
+                      className='absolute bottom-3 
+                      right-3 p-3 rounded-full bg-red-500 text-xl 
+                      cursor-pointer outline-none hover:shadow-md duration-500 transition-all 
+                      ease-in-out' 
+                      onClick={deleteImage}
+                    >
+                      <MdDelete className='text-white' />
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+            )}
           </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateContainer
+export default CreateContainer;
