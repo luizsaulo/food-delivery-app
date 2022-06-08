8bebc5d0077c59737a9eb1170cb5ba26f6b50eed
Luiz <luizsaulo@yahoo.com.br>
2022-06-08 16:56:55 -0300

    implementation of scrollbar, file loading function in the main container from the rowcontainer and their proper style config

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HomeContainer from './HomeContainer';
 import { motion } from "framer-motion";
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import RowContainer from "./RowContainer";
+import { useStateValue } from '../context/StateProvider';
 
 
 const MainContainer = () => {
+  const [{foodItems}, dispatch] = useStateValue();
+  const [scrollValue, setScrollValue] = useState(0)
+
+  useEffect(() => {}, [scrollValue])
+
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <HomeContainer />     
@@ -24,19 +30,27 @@ const MainContainer = () => {
               whileTap={{ scale: 0.75 }} 
               className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer 
               transition-all duration-100 ease-in-out hover:shadow-lg flex 
-              items-center justify-center">
+              items-center justify-center"
+              onClick={() => setScrollValue(-200)}
+            >
                 <MdChevronLeft className="text-lg text-white"/>
               </motion.div>
             <motion.div 
               whileTap={{ scale: 0.75 }} 
               className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer 
               transition-all duration-100 ease-in-out hover:shadow-lg flex 
-              items-center justify-center">
+              items-center justify-center"
+              onClick={() => setScrollValue(200)}
+            >
                 <MdChevronRight className="text-lg text-white"/>
               </motion.div>
           </div>
         </div>
-        <RowContainer flag={true} />
+        <RowContainer
+          scrollValue={scrollValue}      
+          flag={true} 
+          data = {foodItems?.filter((n) => n.category === 'fruits')} 
+        />
       </section> 
     </div>
   );
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { MdShoppingBasket } from 'react-icons/md';
 import { motion } from 'framer-motion';
 
-const RowContainer = ({flag}) => {
+const RowContainer = ({ flag, data, scrollValue }) => {
+    const rowContainer = useRef()
+    useEffect(() => {
+        rowContainer.current.scrollLeft += scrollValue;
+    }, [scrollValue])
   return (
-    <div 
-        className={`w-full my-12  ${
-            flag ? 'overflow-x-scroll' : 'overflow-x-hidden'
+    <div    
+        ref={rowContainer}    
+        className={`w-full flex items-center gap-3 my-12 scroll-smooth  ${
+            flag ? 'overflow-x-scroll scrollbar-none' : 'overflow-x-hidden flex-wrap'
         }`}
     >
-        <div className='w-300 md:w-340 h-auto bg-cardOverlay rounded-lg p-2 my-12 backdrop-blur-lg hover:drop-shadow-lg'>
-            <div className='w-full flex items-center justify-between'>
+        {data && data.map(item => (
+            <div                
+                key={item?.id} 
+                className='w-300 h-[225px] min-w-[300px] md:w-340 md:min-w-[340px]  
+                bg-cardOverlay rounded-lg p-2 my-12 backdrop-blur-lg 
+                hover:drop-shadow-lg flex flex-col items-center justify-between'
+            >
+                <div className='w-full flex items-center justify-between'>
                 <motion.img whileHover={{ scale: 1.2 }}
-                    src='' 
+                    src={item?.imageURL}
                     alt='' 
                     className='w-40 -mt-8 drop-shadow-2xl' 
                 />
@@ -24,16 +35,17 @@ const RowContainer = ({flag}) => {
 
             <div className='w-full flex flex-col items-end justify-end'>
                 <p className='text-textColor font-semibold text-base md:text-lg'>
-                    Chocolate and Vanilla
+                    {item?.title}
                 </p>
-                <p className='mt-1 text-sm text-gray-500'>45 calories</p>
+                <p className='mt-1 text-sm text-gray-500'>{item?.calories} calories</p>
                 <div className='flex items-center gap-8'>
                     <p className='text-lg text-headingColor font-semibold'>
-                        <span className='text-sm text-red-500'>$</span>5.25
+                        <span className='text-sm text-red-500'>$</span>{item?.price}
                     </p>
                 </div>
             </div>
         </div>
+        ))}
     </div>
   )
 }
