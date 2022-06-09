6ea7ce23d8b6fbbcb7a830698bf74fbe5b302e52
Luiz <luizsaulo@yahoo.com.br>
2022-06-09 10:29:30 -0300

    creating a menu container component that will display the menu according to the category of food items

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -17,9 +17,9 @@ const RowContainer = ({ flag, data, scrollValue }) => {
         {data && data.map(item => (
             <div                
                 key={item?.id} 
-                className='w-300 h-[225px] min-w-[300px] md:w-340 md:min-w-[340px]  
-                bg-cardOverlay rounded-lg p-2 my-12 backdrop-blur-lg 
-                hover:drop-shadow-lg flex flex-col items-center justify-between'
+                className='w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  
+                bg-cardOverlay rounded-lg py-2 px-4 my-12 backdrop-blur-lg 
+                hover:drop-shadow-lg flex flex-col items-center justify-evenly relative'
             >
                 <div className='w-full flex items-center justify-between'>
                 <motion.img whileHover={{ scale: 1.2 }}
