73c6ccae6b6e8b661e044398368aa8d7766ba9c7
Luiz <luizsaulo@yahoo.com.br>
2022-06-10 10:32:22 -0300

    finishing the overall layout of the card component adjusting the style, now we must build the logics

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MdOutlineKeyboardBackspace } from 'react-icons/md';
 import { RiRefreshFill } from 'react-icons/ri';
+import { BiMinus, BiPlus } from 'react-icons/bi';
 import { motion } from 'framer-motion';
 
 
@@ -26,7 +27,70 @@ const CartContainer = () => {
         </div>
         {/* bottom section */}
         <div className='w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col'>
+            {/* cart-item section */}
+            <div className='w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll
+            scrollbar-none'>
+                {/* cart item */}
+                <div className='w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2'>
+                    <img 
+                    src=''
+                    className='w-20 h-20 max-w-[60px] rounded-full object-contain'
+                    alt=''
+                    />
 
+                    {/* name section */}
+                    <div className='flex flex-col gap-2'>
+                        <p className='text-base text-gray-50'>Chocolate vanilla</p>
+                        <p className='text-sm block text-gray-300 font-semibold'>$8.25</p>
+                    </div>
+
+                    {/* button section */}
+                    <div className='group flex items-center gap-2 ml-auto cursor-pointer'>
+                        <motion.div whileTap={{ scale: 0.75 }}>
+                            <BiMinus className='text-gray-50' />
+                        </motion.div>
+
+                        <p className='w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center'>
+                           1 
+                        </p>
+
+                        <motion.div whileTap={{ scale: 0.75 }}>
+                            <BiPlus className='text-gray-50' />                            
+                        </motion.div>
+                    </div>
+                </div>
+            </div>
+
+            {/* cart total section */}
+            <div className='w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center
+            justify-evenly px-8 py-2'>
+                <div className='w-full flex items-center justify-between'>
+                    <p className='text-gray-400 text-lg'>Sub Total</p>
+                    <p className='text-gray-400 text-lg'>$ 8.25</p>
+                </div>
+                <div className='w-full flex items-center justify-between'>
+                    <p className='text-gray-400 text-lg'>Delivery</p>
+                    <p className='text-gray-400 text-lg'>$ 2.25</p>
+                </div>
+
+                <div className='w-full border-b border-gray-600 my-2'>
+
+                </div>
+
+                <div className='w-full flex items-center justify-between'>
+                    <p className='text-gray-200 text-xl font-semibold'>Total</p>
+                    <p className='text-gray-200 text-xl font-semibold'>$10.50</p>
+                </div>
+
+                <motion.button
+                    whileTap={{ scale: 0.8 }}
+                    type='button'
+                    className='w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 
+                    text-gray-50 text-lg my-2 hover:shadow-lg transition-all duration-150 ease-out'
+                >
+                    Checkout
+                </motion.button>
+            </div>
         </div>
     </div>
   )
