540b3c2f3f6ec8cc329514adf356b964aa899737
Luiz <luizsaulo@yahoo.com.br>
2022-06-13 14:56:29 -0300

    builind the quantity effect and state for when you click the plus and minus button the quantity of items change, including the amount of the total expense

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { MdOutlineKeyboardBackspace } from 'react-icons/md';
 import { RiRefreshFill } from 'react-icons/ri';
-import { BiMinus, BiPlus } from 'react-icons/bi';
 import { motion } from 'framer-motion';
 import { useStateValue } from '../context/StateProvider';
 import { actionType } from '../context/reducer';
 import EmptyCart from '../img/emptyCart.svg';
+import CartItem from './CartItem';
 
 const CartContainer = () => {
 
@@ -50,34 +50,7 @@ const CartContainer = () => {
             scrollbar-none'>
                 {/* cart item */}
                 {cartItems && cartItems.map(item => (
-                    <div key={item.id} className='w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2'>
-                    <img 
-                        src={item?.imageURL}
-                        className='w-20 h-20 max-w-[60px] rounded-full object-contain'
-                        alt=''
-                    />
-
-                    {/* name section */}
-                    <div className='flex flex-col gap-2'>
-                        <p className='text-base text-gray-50'>{item?.title}</p>
-                        <p className='text-sm block text-gray-300 font-semibold'>$ {item?.price}</p>
-                    </div>
-
-                    {/* button section */}
-                    <div className='group flex items-center gap-2 ml-auto cursor-pointer'>
-                        <motion.div whileTap={{ scale: 0.75 }}>
-                            <BiMinus className='text-gray-50' />
-                        </motion.div>
-
-                        <p className='w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center'>
-                           {item.qty}
-                        </p>
-
-                        <motion.div whileTap={{ scale: 0.75 }}>
-                            <BiPlus className='text-gray-50' />                            
-                        </motion.div>
-                    </div>
-                </div>
+                    <CartItem key={item.id} item={item} />
                 ))}
             </div>
 
